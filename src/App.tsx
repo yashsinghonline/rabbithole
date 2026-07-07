69bc865771e8da704ecec7195e01a97fb41c75b0
yashsinghonline <yashsinghonwork@gmail.com>
2026-07-07 10:15:17 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+
+import Home from "./pages/Home";
+import LoadingScreen from "./components/LoadingScreen";
+import GlitchOverlay from "./components/GlitchOverlay"; // <-- Add this
+
+import { depthStore } from "./state/depthStore";
+
+export default function App() {
+  const [introDone, setIntroDone] = useState(depthStore.introSeen);
+
+  if (!introDone) {
+    return (
+      <LoadingScreen
+        onDone={() => {
+          depthStore.introSeen = true;
+          setIntroDone(true);
+        }}
+      />
+    );
+  }
+
+  return (
+    <>
+      {/* Global Glitch Effect */}
+      <GlitchOverlay />
+
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </HashRouter>
+    </>
+  );
+}
